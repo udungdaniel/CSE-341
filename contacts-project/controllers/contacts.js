853de42a1ac08956776e63373c7fa4a34f640702
udungdaniel <danielalfred783@gmail.com>
2026-05-24 19:30:59 +0100

    Updated contacts.rest with valid MongoDB IDs

--- contacts-project/controllers/contacts.js
+++ contacts-project/controllers/contacts.js
@@ -37,7 +37,83 @@ const getSingle = async (req, res) => {
   }
 };
 
+const createContact = async (req, res) => {
+  try {
+    const db = await mongodb.initDb();
+
+    const contact = {
+      firstName: req.body.firstName,
+      lastName: req.body.lastName,
+      email: req.body.email,
+      favoriteColor: req.body.favoriteColor,
+      birthday: req.body.birthday
+    };
+
+    const response = await db.collection('contacts').insertOne(contact);
+
+    if (response.acknowledged) {
+      res.status(201).json(response);
+    } else {
+      res.status(500).json(response.error || 'Some error occurred while creating the contact.');
+    }
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+};
+
+const updateContact = async (req, res) => {
+  try {
+    const contactId = new ObjectId(req.params.id);
+
+    const contact = {
+      firstName: req.body.firstName,
+      lastName: req.body.lastName,
+      email: req.body.email,
+      favoriteColor: req.body.favoriteColor,
+      birthday: req.body.birthday
+    };
+
+    const db = await mongodb.initDb();
+
+    const response = await db.collection('contacts').replaceOne(
+      { _id: contactId },
+      contact
+    );
+
+    if (response.modifiedCount > 0) {
+      res.status(204).send();
+    } else {
+      res.status(500).json(response.error || 'Some error occurred while updating the contact.');
+    }
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+};
+
+const deleteContact = async (req, res) => {
+  try {
+    const contactId = new ObjectId(req.params.id);
+
+    const db = await mongodb.initDb();
+
+    const response = await db.collection('contacts').deleteOne({
+      _id: contactId
+    });
+
+    if (response.deletedCount > 0) {
+      res.status(200).send();
+    } else {
+      res.status(500).json(response.error || 'Some error occurred while deleting the contact.');
+    }
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+};
+
 module.exports = {
   getAll,
-  getSingle
+  getSingle,
+  createContact,
+  updateContact,
+  deleteContact
 };
--- contacts-project/routes/contacts.js
+++ contacts-project/routes/contacts.js
@@ -1,9 +1,8 @@
 const express = require('express');
-const { ObjectId } = require('mongodb');
 
 const router = express.Router();
 
-const mongodb = require('../mongodb');
+const contactsController = require('../controllers/contacts');
 
 /**
  * @swagger
@@ -14,137 +13,105 @@ const mongodb = require('../mongodb');
  *       200:
  *         description: Success
  */
-router.get('/', async (req, res) => {
-  try {
-    const db = await mongodb.initDb();
-
-    const result = await db.collection('contacts').find();
-
-    const contacts = await result.toArray();
-
-    res.status(200).json(contacts);
-  } catch (err) {
-    console.error(err);
-    res.status(500).json({ message: err.message });
-  }
-});
+router.get('/', contactsController.getAll);
 
 /**
  * @swagger
  * /contacts/{id}:
  *   get:
  *     summary: Get single contact
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         description: Contact ID
+ *         schema:
+ *           type: string
+ *     responses:
+ *       200:
+ *         description: Success
  */
-router.get('/:id', async (req, res) => {
-  try {
-    const contactId = new ObjectId(req.params.id);
-
-    const db = await mongodb.initDb();
-
-    const result = await db
-      .collection('contacts')
-      .find({ _id: contactId });
-
-    const contacts = await result.toArray();
-
-    res.status(200).json(contacts[0]);
-  } catch (err) {
-    console.error(err);
-    res.status(500).json({ message: err.message });
-  }
-});
+router.get('/:id', contactsController.getSingle);
 
 /**
  * @swagger
  * /contacts:
  *   post:
  *     summary: Create new contact
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               firstName:
+ *                 type: string
+ *               lastName:
+ *                 type: string
+ *               email:
+ *                 type: string
+ *               favoriteColor:
+ *                 type: string
+ *               birthday:
+ *                 type: string
+ *     responses:
+ *       201:
+ *         description: Contact created
  */
-router.post('/', async (req, res) => {
-  try {
-    const db = await mongodb.initDb();
-
-    const contact = {
-      firstName: req.body.firstName,
-      lastName: req.body.lastName,
-      email: req.body.email,
-      favoriteColor: req.body.favoriteColor,
-      birthday: req.body.birthday
-    };
-
-    const response = await db.collection('contacts').insertOne(contact);
-
-    res.status(201).json({
-      message: 'Contact created',
-      id: response.insertedId
-    });
-  } catch (err) {
-    console.error(err);
-    res.status(500).json({ message: err.message });
-  }
-});
+router.post('/', contactsController.createContact);
 
 /**
  * @swagger
  * /contacts/{id}:
  *   put:
  *     summary: Update contact
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         description: Contact ID
+ *         schema:
+ *           type: string
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               firstName:
+ *                 type: string
+ *               lastName:
+ *                 type: string
+ *               email:
+ *                 type: string
+ *               favoriteColor:
+ *                 type: string
+ *               birthday:
+ *                 type: string
+ *     responses:
+ *       204:
+ *         description: Contact updated
  */
-router.put('/:id', async (req, res) => {
-  try {
-    const contactId = new ObjectId(req.params.id);
-
-    const db = await mongodb.initDb();
-
-    const contact = {
-      firstName: req.body.firstName,
-      lastName: req.body.lastName,
-      email: req.body.email,
-      favoriteColor: req.body.favoriteColor,
-      birthday: req.body.birthday
-    };
-
-    const response = await db.collection('contacts').replaceOne(
-      { _id: contactId },
-      contact
-    );
-
-    if (response.modifiedCount > 0) {
-      res.status(204).send();
-    } else {
-      res.status(500).json(response.error || 'Some error occurred.');
-    }
-  } catch (err) {
-    console.error(err);
-    res.status(500).json({ message: err.message });
-  }
-});
+router.put('/:id', contactsController.updateContact);
 
 /**
  * @swagger
  * /contacts/{id}:
  *   delete:
  *     summary: Delete contact
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         description: Contact ID
+ *         schema:
+ *           type: string
+ *     responses:
+ *       200:
+ *         description: Contact deleted
  */
-router.delete('/:id', async (req, res) => {
-  try {
-    const contactId = new ObjectId(req.params.id);
-
-    const db = await mongodb.initDb();
-
-    const response = await db.collection('contacts').deleteOne({
-      _id: contactId
-    });
-
-    if (response.deletedCount > 0) {
-      res.status(200).send();
-    } else {
-      res.status(500).json(response.error || 'Some error occurred.');
-    }
-  } catch (err) {
-    console.error(err);
-    res.status(500).json({ message: err.message });
-  }
-});
+router.delete('/:id', contactsController.deleteContact);
 
 module.exports = router;
